d7f934e721a6a3a9d5aa0dabc54adab8539bc723
Mohd Azfar <62957738+mo-azfar@users.noreply.github.com>
2020-04-18 22:53:31 +0800

    Update AgentITSMConfigItemEdit.pm
    Using custom unique field API instead of direct unique checking

--- Custom/Kernel/Modules/AgentITSMConfigItemEdit.pm
+++ Custom/Kernel/Modules/AgentITSMConfigItemEdit.pm
@@ -2,53 +2,55 @@
 ##and above the BLOCK OF # get the config option for the name regex checks
 ##add these code
 		
-		
-		#############FOR CUSTOM FIELD UNIQUE CHECK##########################
-		if (
+	#############FOR CUSTOM FIELD UNIQUE CHECK##########################
+	if (
             IsStringWithData( $Version->{Name} )
             && $ConfigObject->Get('ITSMConfigItem::UniqueField')
             )
         {
 			
-			#get current class name
-			my $CurrentClass = $GeneralCatalogObject->ItemGet(
-			ItemID => $ConfigItem->{ClassID},
-			);
+		#get current class name
+		my $CurrentClass = $GeneralCatalogObject->ItemGet(
+		ItemID => $ConfigItem->{ClassID},
+		);
 			
-            my @Mappings = @{ $ConfigObject->Get('ITSMConfigItem::UniqueFieldValue') };
+            	my @Mappings = @{ $ConfigObject->Get('ITSMConfigItem::UniqueFieldValue') };
             
-            foreach my $Mapping (@Mappings)
-            { 
-                my($Class, $DefinitionField, $FieldNumber) = split(/::/, $Mapping, 3);
-                next if $Class ne $CurrentClass->{Name};
-                next if $FieldNumber ne 1;
-                my $CurrentUniqueValue = $Version->{XMLData}->[1]->{Version}->[1]->{$DefinitionField}->[$FieldNumber]->{Content};
-                next if $CurrentUniqueValue eq '';
-                my $DuplicateCustomFields = $ConfigItemObject->ConfigItemSearchExtended(
-					ClassIDs => [$ConfigItem->{ClassID}],
-					What => [                                                # (optional)
-						# each array element is a and condition
-						{			
-							"[%]{'Version'}[%]{'$DefinitionField'}[%]{'Content'}" => [$CurrentUniqueValue],				
-						}
-					],
-		
-					);
-                
-                   @{$DuplicateCustomFields} = map {$_} grep { $_ ne $ConfigItem->{ConfigItemID} } @{$DuplicateCustomFields};
-                    
-                    if ( IsArrayRefWithData($DuplicateCustomFields) ) 
-                    {
-                        my $found_cid = join( ', ', @{$DuplicateCustomFields} );
-                        return $LayoutObject->ErrorScreen(
-                        Message => $LayoutObject->{LanguageObject}
-                        ->Translate( "The $DefinitionField: $CurrentUniqueValue is already in use by the ConfigItemID(s): $found_cid" ),
-                        Comment => Translatable('Please assign new value'),
-                        );
-                    }
-            }
+            	foreach my $Mapping (@Mappings)
+            	{ 
+                	my($Class, $DefinitionField, $FieldNumber) = split(/::/, $Mapping, 3);
+                	next if $Class ne $CurrentClass->{Name};
+                	next if $FieldNumber ne 1;
+                	my $CurrentUniqueValue = $Version->{XMLData}->[1]->{Version}->[1]->{$DefinitionField}->[$FieldNumber]->{Content};
+                	next if $CurrentUniqueValue eq '';
+                	
+                	my $DuplicateFieldValues = $Kernel::OM->Get('Kernel::System::ITSMUniqueField')->UniqueFieldCheck(
+                	    ConfigItemID => $ConfigItem->{ConfigItemID},
+                	    FieldName    => $DefinitionField,
+                	    FieldValue   => $CurrentUniqueValue,
+                	    ClassID      => $ConfigItem->{ClassID},
+                	);
+                	
+                	
+                	 # stop processing if the name is not unique
+                	if ( IsArrayRefWithData($DuplicateFieldValues) ) 
+                	{
+    
+			    $AllRequired = 0;
+    
+			    # build a string of all duplicate IDs
+			    my $found_cid = join( ', ', @{$DuplicateFieldValues} );
+
+			    return $LayoutObject->ErrorScreen(
+				Message => $LayoutObject->{LanguageObject}
+				->Translate( "The $DefinitionField: $CurrentUniqueValue is already in use by the ConfigItemID(s): $found_cid" ),
+				Comment => Translatable('Please assign new value'),
+				);
+               		 }
+     
+            	}
 	
-		}
-		#############END FOR CUSTOM FIELD UNIQUE CHECK##########################
+	}
+	#############END FOR CUSTOM FIELD UNIQUE CHECK##########################
 		
 		
